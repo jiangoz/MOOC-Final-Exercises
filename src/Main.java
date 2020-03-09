c1df67cf139e65cd33031fdd6863414e4ce57c3e
Yi J <yjiang2001@gmail.com>
2020-03-08 21:04:46 -0400

    removed comment

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,8 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        // Write your main program here. Implementing your own classes will be very useful.
 
         Random rand = new Random();
-
         Program skiProgram = new Program(rand);
         skiProgram.start();
 
